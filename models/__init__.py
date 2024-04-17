6eeb65785c3e3a4146da237b18ec0c5d03fd4613
gboigwe <geakande@gmail.com>
2024-04-17 18:48:04 +0100

    Updated the code

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-"""
-#!/Users/mistarkelly/vagrant_project/My-Projects/ALX-ONLY/AirBnB_clone_v2/.venv/bin/python3
-This module instantiates an object of class DBStorage
-if the environment variable HBNB_TYPE_STORAGE is equal to db,
-otherwise it instantiates an object of class FileStorage
-"""
-from os import getenv
-from models.engine.db_storage import DBStorage
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    storage = DBStorage()
-else:
-    storage = FileStorage()
+
+storage = FileStorage()
 storage.reload()
