d46eec00e09c0417d451ed3da59212d3462eba0e
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-05-18 06:25:13 +0100

    models  folder update fix 4

--- models/state.py
+++ models/state.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-"""
-State class for creating State table
-#!/Users/mistarkelly/vagrant_project/My-Projects/ALX-ONLY/AirBnB_clone_v2/.venv/bin/python3
-
-"""
+""" holds class State"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
 from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """
-         state table.
-         would be a Table in the database.
-     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
-    
-    @property
-    def cities(self):
-        """
-            Getter method for cities
-            Returns a list of City objects linked to the current State
-        """
-        from models import storage
-        cities = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                cities.append(city)
-        return cities
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+        
